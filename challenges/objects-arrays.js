8252962a7d7705e3bb4ec163d42b02711d97ccab
noahfranco <noahfra@gmail.com>
2019-06-14 12:40:15 -0400

    finished objects in objects-arrays.js file but it's given me an error

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -8,27 +8,55 @@
 
 // tyrannosaurus, carnivorous, 7000kg, 12m, Late Cretaceous
 
+ const tyrannosaurus1 = {
+  name: tyrannosaurus,
+  diet: carnivorous, 
+  weight: 7000 + kg,
+  length: 12 + m,
+  period: Late + " " + Cretaceous,
+  roarMethod: function() {
+    return "RAWERSRARARWERSARARARRRR!" 
+ }
+};
+  
+
 // stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
 
+const stegosaurus1 = {
+  name: herbivorous,
+  diet: carnivorous, 
+  weight: 2000 + kg,
+  length: 9 + m,
+  period: Late + " " + Jurassic
+};
+
 // velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceous
+
+const velociraptor1 = {
+  name:  velociraptor,
+  diet: carnivorous, 
+  weight: 15 + kg,
+  length: 1.8 + m,
+  period: Late + " " + Cretaceous
+};
 
 // Using your dinosaur objects, log answers to these questions:
 
 // How much did tyrannosaurus weigh?
-console.log();
+console.log(tyrannosaurus1.weight);
 
 // What was the diet of a velociraptor?
-console.log();
+console.log(velociraptor1.diet);
 
 // How long was a stegosaurus?
-console.log();
+console.log(stegosaurus1.length);
 
 // What time period did tyrannosaurus live in?
-console.log();
+console.log(tyrannosaurus1.period);
 
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
-console.log();
+console.log(tyrannosaurus1.roarMethod);
 
 
 // ==== Arrays ====
